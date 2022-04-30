b411deeb2a57e11a732704909e89100fa91a606e
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2022-04-30 14:44:18 +0200

    Switched on generate documentation for WebApi.

--- QuickTemplate.WebApi/Program.cs
+++ QuickTemplate.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "QuickTemplate API",
+        Description = "An API for managing our system",
+        Contact = new OpenApiContact
+        {
+            Name = "HTBLA Leonding",
+            Url = new Uri("https://www.htl-leonding.at/")
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
