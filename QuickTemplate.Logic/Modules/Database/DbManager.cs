1a2d15189f7beb3fd6a3b18bb5c136b61ce82d70
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2022-05-21 10:04:28 +0200

    Added DbManager.

---
+++ QuickTemplate.Logic/Modules/Database/DbManager.cs
@@ -0,0 +1,28 @@
+﻿#if DEVELOP_ON
+using QuickTemplate.Logic.DataContext;
+
+namespace QuickTemplate.Logic.Modules.Database
+{
+    public static class DbManager
+    {
+        public static Task DeleteDatabaseAsync()
+        {
+            var dbContext = new ProjectDbContext();
+
+            return dbContext.Database.EnsureDeletedAsync();
+        }
+        public static Task MigrateDatabaseAsync()
+        {
+            var dbContext = new ProjectDbContext();
+
+            return dbContext.Database.MigrateAsync();
+        }
+        public static Task CreateDatabaseAsync()
+        {
+            var dbContext = new ProjectDbContext();
+
+            return dbContext.Database.EnsureCreatedAsync();
+        }
+    }
+}
+#endif
