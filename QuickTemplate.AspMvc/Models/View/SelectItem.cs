a1125bde07a1d77fb3890f232fe9b104ed5ea33d
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2022-06-21 22:55:03 +0200

    Added SelectItem.

---
+++ QuickTemplate.AspMvc/Models/View/SelectItem.cs
@@ -0,0 +1,11 @@
+﻿//@BaseCode
+//MdStart
+namespace QuickTemplate.AspMvc.Models.View
+{
+    public class SelectItem
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
+//MdEnd
